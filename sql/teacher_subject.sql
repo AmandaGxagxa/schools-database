173e8eb168a3a894d76f4461da3a0ae49da8cc59
AmandaGxagxa <amandagxagxa@gmail.com>
2020-09-02 12:58:55 +0200

    my first commit

--- sql/teacher_subject.sql
+++ sql/teacher_subject.sql
@@ -16,6 +16,10 @@ insert into teacher_subject (teacher_id, subject_id) values (4, 5);
 insert into teacher_subject (teacher_id, subject_id) values (5, 6);
 insert into teacher_subject (teacher_id, subject_id) values (5, 4);
 insert into teacher_subject (teacher_id, subject_id) values (4, 3);
+insert into teacher_subject (teacher_id, subject_id) values (4, 1);
+insert into teacher_subject (teacher_id, subject_id) values (2, 3);
+insert into teacher_subject (teacher_id, subject_id) values (3, 1);
+
 
 --joining table teacher to the subject one to see which teacher is teaching which subject
 select * from teacher
@@ -30,3 +34,13 @@ from teacher
 	join subject on teacher_subject.subject_id = subject.id
 where
 	subject.name = 'Geography';
+
+--	finding all the teachers that teaches more tn 3 subjects
+select
+   teacher.first_name, teacher.last_name, teacher.email, count(*)
+from teacher
+	join teacher_subject on teacher.id = teacher_subject.teacher_id
+	join subject on teacher_subject.subject_id = subject.id
+group by
+   teacher.first_name, teacher.last_name, teacher.email
+having count(*) >= 3;
