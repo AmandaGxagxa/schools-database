2ea92b7b3c39dcb20444763b6523b3f5c65f8cbf
AmandaGxagxa <amandagxagxa@gmail.com>
2020-09-03 14:56:50 +0200

    commiting

--- sql/teacher_subject.sql
+++ sql/teacher_subject.sql
@@ -45,21 +45,20 @@ group by
    teacher.first_name, teacher.last_name, teacher.email
 having count(*) >= 3;
 -- finding how many subjects its teacher teaches.
-select
-   teacher.first_name, teacher.last_name, teacher.email, count(*)
-from teacher
-	join teacher_subject on teacher.id = teacher_subject.teacher_id
-	join subject on teacher_subject.subject_id = subject.id
-group by
-   teacher.first_name, teacher.last_name, teacher.email
-
+--select
+--   teacher.first_name, teacher.last_name, teacher.email, count(*)
+--from teacher
+--	join teacher_subject on teacher.id = teacher_subject.teacher_id
+--	join subject on teacher_subject.subject_id = subject.id
+--group by
+--   teacher.first_name, teacher.last_name, teacher.email
 
 --   finding all the subjects taught by a given teacher.
 
 select
 	subject.*
 from subject
- 	join teacher_subject on teacher.id = teacher_subject.teacher_id
-	join subject on teacher_subject.subject_id = subject.id
+ 	join teacher_subject on subject.id = teacher_subject.subject_id
+	join teacher on teacher_subject.teacher_id = teacher.id
 where
 	teacher.first_name = 'Siba';
